b7ecc2abc7f1646b96079e9be23c816908c74f2e
a124557 <av@alivirani.com>
2023-02-15 21:53:10 -0500

    Create 'data' dir if it does not exist with PHP

--- save_data.php
+++ save_data.php
@@ -1,4 +1,11 @@
 <?php
+// set a path to the directory './data'
+$dir = './data';
+// check if the directory exists
+If (!is_dir($dir)) {
+	// create the directory if it does not exist
+	mkdir($dir);
+}
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
 $filename = "data/".time().$_POST['filename'];
