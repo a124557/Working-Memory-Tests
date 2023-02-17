5e074da0318361bcf084c77defeb16f854b71614
a124557 <av@alivirani.com>
2023-02-17 16:20:43 -0500

    Change behaviour of filename

--- save_data.php
+++ save_data.php
@@ -9,6 +9,11 @@ If (!is_dir($dir)) {
 // the $_POST[] array will contain the passed in filename and data
 // the directory "data" is writable by the server (chmod 777)
 $filename = "data/".$_POST['filename'];
+$num = 1;
+while(file_exists($filename)) {
+	$filename = "data/".$_POST['filename'];
+	$num++;
+}
 $data = $_POST['filedata'];
 // write the file to disk
 file_put_contents($filename, $data);
